1d6279ddfcc62cd24e7ff7213a229826fcc0b84e
Aditi <aditi2342006@gmail.com>
2026-08-02 16:52:11 +0530

    Fix API URL for production deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production'
-    ? '/api'
-    : 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
 });
 
-// Automatically attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
